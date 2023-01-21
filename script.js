4643660eacb6183d1964dcbcab36309d56a36f91
Inna Stukalova <protege.moi085@gmail.com>
2023-01-21 14:14:00 +0300

    8 kyu: string repeat

--- script.js
+++ script.js
@@ -6,6 +6,15 @@
 //   return Math.ceil (month / 3);
 // };
 
-function invert(array) {
-  return array.map((number) => number * -1);
+// function invert(array) {
+//   return array.map((number) => number * -1);
+// }
+
+function repeatStr (n, s) {
+  return s.repeat(n);
 }
+
+console.log(repeatStr(5, 'lol'));
+
+
+
