6d31abd2d0267bdcf8e763779a2a7995e0373c2b
Inna Stukalova <protege.moi085@gmail.com>
2024-01-31 18:49:13 +0300

    8 kyu: students final grade

--- script.js
+++ script.js
@@ -46,7 +46,11 @@
 //   }
 
 
-function simpleMultiplication(number) {
-    return number % 2 === 0 ? number * 8 : number * 9;
-}
+// function simpleMultiplication(number) {
+//     return number % 2 === 0 ? number * 8 : number * 9;
+// }
 
+
+function finalGrade (exam, projects) {
+    return exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0;
+  }
