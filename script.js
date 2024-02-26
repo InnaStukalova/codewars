00b825facd4e01af5592dfb6724c285b36641428
Inna Stukalova <protege.moi085@gmail.com>
2024-02-26 18:19:10 +0300

    7 kyu: 16+18=214

--- script.js
+++ script.js
@@ -109,6 +109,19 @@
 // };
 
 
-function mergeArrays(arr1, arr2) {
-  return [...new Set([...arr1, ...arr2])].sort((a, b) => a - b); 
+// function mergeArrays(arr1, arr2) {
+//   return [...new Set([...arr1, ...arr2])].sort((a, b) => a - b); 
+// }
+
+
+function add(num1, num2) {
+    num1 = num1.toString().split('').reverse();
+    num2 = num2.toString().split('').reverse();
+    let max = Math.max(num1.length, num2.length);
+    let newArr = [];
+    for (let i = max - 1; i >= 0; i--) {
+        newArr.push(Number(num1[i] || 0) + Number((num2[i]) || 0));
+    }
+    let result = newArr.reduce((acc, ell) => acc.toString() + ell.toString());
+    return Number(result);
 }
